571e509db65a37b5d987ba63c2a86102835ceb81
Cyril <37065900+cyril442@users.noreply.github.com>
2019-01-25 14:37:21 +0100

    Fixed setBackground, setWidth, setIconOrNot
    setBackgroundColor => OK (Color of the list_item is changing according to the Mood Selected)
    setWidth => OK ( Width of the list_item is changing according to the Mood Selected) 
    setIconOrNor => OK (If Comment not "null", it makes the ImageButton VISIBLE. else, ImageButton GONE)

--- MoodHistoryAdapter.java
+++ MoodHistoryAdapter.java
@@ -9,21 +9,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 public final class MoodHistoryAdapter extends BaseAdapter {
 
 
-    private   Context context ;
-    private   LayoutInflater inflater;
+    private Context context;
+    private LayoutInflater inflater;
     //   private    ArrayList<MoodData> moodDataList = ArrayList<>;
-    private ArrayList moodDataList = new ArrayList();
+   // private ArrayList moodDataList = new ArrayList();
+    private List<MoodData> listOfMoodData;
     private int screenWidth;
-
 
 
     public MoodHistoryAdapter(Context baseContext, List<MoodData> moodDataList) {
@@ -31,12 +31,14 @@ public final class MoodHistoryAdapter extends BaseAdapter {
         context = baseContext;
         inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
 
+        this.listOfMoodData = moodDataList;
+
 
     }
 
     @Nullable
     public Object getItem(int position) {
-        return this.moodDataList.get(position);
+        return this.listOfMoodData.get(position);
     }
 
     public long getItemId(int position) {
@@ -45,23 +47,28 @@ public final class MoodHistoryAdapter extends BaseAdapter {
 
     public int getCount() {
 
-        return this.moodDataList.size();
+        return this.listOfMoodData.size();
     }
 
     @Override
     public View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent) {
         View view;
 
+
         if (convertView == null) {
             view = inflater.inflate(R.layout.list_items_row, parent, false);
-            TextView numberOfTheDay = view.findViewById(R.id.textOfTheDay);
-         // switch (moodDataList.get(position)) {
-            switch (position) {
+            TextView numberOfTheDay = view.findViewById(R.id.numberOfTheDay);
+            ImageButton commentIcon = view.findViewById(R.id.commentIcon);
+            // switch (moodDataList.get(position)) {
+            switch (this.listOfMoodData.get(position).MOOD) {
+                //   case 0: //Smiley Sad
+//            switch (position) {
 
                 case 0: //Smiley Sad
                     view.setBackgroundResource(R.color.color_sad);
-                    numberOfTheDay.setMaxWidth(screenWidth()/5);
-                    //display width
+                    numberOfTheDay.setWidth(screenWidth() / 5);
+                    IconOrNot(position, commentIcon);
+                 //   commentIcon.setVisibility(View.GONE);
                     // visible or gone comment icon
                     // on clickable comment icon to open Toast with the message stored in the Database
                     // Appear text "One week ago", "6 days ago", etc...
@@ -70,30 +77,53 @@ public final class MoodHistoryAdapter extends BaseAdapter {
 
                 case 1: //Smiley Disappointed
                     view.setBackgroundResource(R.color.color_disappointed);
-                    numberOfTheDay.setMaxWidth(screenWidth()/4);
+                    numberOfTheDay.setWidth(screenWidth() / 4);
+                    IconOrNot(position, commentIcon);
+                  //  commentIcon.setVisibility(View.GONE);
                     break;
 
                 case 2: //Smiley Normal
                     view.setBackgroundResource(R.color.color_normal);
-                    numberOfTheDay.setMaxWidth(screenWidth()/3);
+                    numberOfTheDay.setWidth(screenWidth() / 3);
+                    IconOrNot(position, commentIcon);
+                   // commentIcon.setVisibility(View.GONE);
                     break;
 
                 case 3: //Smiley Happy
                     view.setBackgroundResource(R.color.color_happy);
-                    numberOfTheDay.setMaxWidth(screenWidth()/2);
+                    numberOfTheDay.setWidth(screenWidth() / 2);
+                    IconOrNot(position, commentIcon);
+                   // commentIcon.setVisibility(View.GONE);
                     break;
 
                 case 4: //Smiley Super_Happy
                     view.setBackgroundResource(R.color.color_super_happy);
-                    numberOfTheDay.setMaxWidth(screenWidth()/1);
+                    numberOfTheDay.setWidth(screenWidth() /  1);
+                    IconOrNot(position, commentIcon);
+                  //  commentIcon.setVisibility(View.VISIBLE);
                     break;
 
-            }} else {
-                view = convertView;
+                default:
+                    break;
             }
-
-            return view;
+        } else {
+            view = convertView;
         }
+
+        return view;
+    }
+
+    private void IconOrNot(int position, ImageButton commentIcon) {
+
+       String iconOrNot = listOfMoodData.get(position).COMMENT;
+
+       if ( iconOrNot != null ){
+           commentIcon.setVisibility(View.VISIBLE);
+       }
+       else {
+           commentIcon.setVisibility(View.GONE);
+       }
+    }
 
     private int screenWidth() {
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -103,9 +133,8 @@ public final class MoodHistoryAdapter extends BaseAdapter {
 
             screenWidth = displayMetrics.widthPixels;
         }
-            return screenWidth;
-        }
-
-
-
+        return screenWidth;
     }
+
+
+}
